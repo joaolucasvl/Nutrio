71958f3add952df9c1d0c6828a6ff37183714fa9
joaolucasvl <joaovitorlucasdev@gmail.com>
2025-04-14 23:12:00 -0300

    Teste NPM

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import TBM from "./pages/tbm";
 import IMC from "./pages/imc";
@@ -9,15 +9,14 @@ import Contato from "./pages/contato";
 
 const Rotas = () => {
     return (
-        <Router>
+        <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/tbm" element={<TBM />} />
                 <Route path="/imc" element={<IMC />} />
                 <Route path="/contato" element={<Contato />} />
-                
             </Routes>
-        </Router>
+        </BrowserRouter>
     );
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
-import talwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite'  
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/Nutrio',
+export default defineConfig({ 
   plugins: [
     react(),
-    talwindcss()
+    tailwindcss()  
   ],
-
 })
