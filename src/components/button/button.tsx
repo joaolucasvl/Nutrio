112fbf015f2fe955096a4e369185529d92de49b9
João Vitor Lucas de Oliveira <joao.vitor@takerit.com>
2025-04-15 14:15:27 -0300

    Finalização pagina contato

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -11,7 +11,7 @@ export const Button: FC<ButtonProps> = ({ type, ...props }) => {
       {...props}
       className="w-full rounded-md bg-blue-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-none active:bg-slate-700 hover:bg-blue-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none "
     >
-      {type === "submit" ? "Calcular" : "Enviar"}
+      {type === "submit" ? "Calcular" : "Enviar" }
     </button>
   );
 };
--- src/pages/contato/index.tsx
+++ src/pages/contato/index.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { Header } from "../../components/header/Header";
 import { Footer } from "../../components/footer/Footer";
+import { useState } from "react";
+import { Input } from "../../components/input/Input";
 
 function Contato() {
+    function handleSubmit(){
+        const [nome, setNome] = useState("")
+        const [email, setEmail] = useState("")
+        const [area, setArea] = useState("")
+    }
     return (
         <div className="flex flex-col min-h-screen">
             <Header/>
@@ -13,57 +20,32 @@ function Contato() {
                         Está buscando orientação profissional ou apoio em um projeto específico? Preencha o formulário abaixo e nossa equipe entrará em contato com você o mais breve possível.
                     </p>
 
-                    <form className="space-y-4 mb-40">
+                    <form className="space-y-6 mb-40" onSubmit={handleSubmit}>
                         <div>
-                            <label className="block text-sm font-medium text-gray-700">
+                            <label className="block text-sm font-medium text-gray-700 mb-2">
                                 Nome
                             </label>
-                            <input
-                                type="text"
-                                placeholder="Seu nome completo"
-                                className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-green-400"
-                            />
+                            <Input placeholder="Digite seu nome" name="nome"/>
+                            
                         </div>
 
                         <div>
-                            <label className="block text-sm font-medium text-gray-700">
+                            <label className="block text-sm font-medium text-gray-700 mb-2">
                                 Email
                             </label>
-                            <input
-                                type="email"
-                                placeholder="seuemail@exemplo.com"
-                                className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-green-400"
-                            />
+                            <Input placeholder="Digite seu e-mail" name="email"/>
+                            
                         </div>
-
                         <div>
-                            <label className="block text-sm font-medium text-gray-700">
+                            <label className="block text-sm font-medium text-gray-700 mb-2">
                                 Área de Interesse
                             </label>
-                            <input
-                                type="text"
-                                placeholder="Ex: Nutrição, Treinamento"
-                                className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-green-400"
-                            />
+                            <Input placeholder="Nutrição, Treinamento..." name="area"/>      
                         </div>
-
-                        <div>
-                            <label className="block text-sm font-medium text-gray-700">
-                                Mensagem
-                            </label>
-                            <textarea
-                                rows={5}
-                                placeholder="Nos conte mais sobre o que você precisa..."
-                                className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-green-400"
-                            ></textarea>
-                        </div>
-
-                        <button
-                            type="submit"
-                            className="bg-green-500 hover:bg-green-600 text-white font-semibold px-6 py-2 rounded-xl shadow-md transition"
-                        >
-                            Solicitar Consultoria
+                        <button type="submit" className="w-full rounded-md bg-green-700 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-none active:bg-slate-700 hover:bg-blue-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ">
+                            Solicitar
                         </button>
+                        
                     </form>
                 </div>
             </div>
